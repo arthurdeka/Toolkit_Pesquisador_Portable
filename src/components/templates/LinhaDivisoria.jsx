3f2f446a595948e6309bdc57644b35146f6f4042
Arthur <arthurdk01@gmail.com>
2024-03-02 16:17:36 -0300

    1.2.1 - envio do dist
    envio dos arquivos de dist para hospedagem da aplicação.

--- src/components/templates/LinhaDivisoria.jsx
+++
@@ -1,9 +0,0 @@
-import ModalImage from "react-modal-image";
-
-function LinhaDivisoria({ caminhoImagem }) {
-  return (
-    <div className="border-corprimaria opacity-40 my-8 mx-4 border-t-4"></div>
-  );
-}
-
-export default LinhaDivisoria;
--- src/utils/capitalizarPalavras.js
+++
@@ -1,6 +0,0 @@
-/* função para padronizar strings em "Todas As Primeiras Letras Maiúsculas" */
-export function capitalizarPalavras(str) {
-    return str.replace(/\b\w/g, function(l) {
-      return l.toUpperCase();
-    });
-  }
