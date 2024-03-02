2b93bef330e633022b5dd6cbce0af78d50906b19
Arthur <arthurdk01@gmail.com>
2024-03-02 18:17:45 -0300

    hotfix - rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Toolkit_Pesquisador_Portable'
+  base: '/Toolkit_Pesquisador_Portable',
+  build: {
+    rollupOptions: {
+      external: ["react", "react-router", "react-router-dom", "react-redux"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
 });
